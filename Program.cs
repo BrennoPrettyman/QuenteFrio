3e5930f345139d6599f44b4ef436867262abe7db
brennoprettyman <brennoprettyman@gmail.com>
2023-06-11 12:41:26 -0300

    Tentativa 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+Console.Clear();
+
+int secret = RandomNumberGenerator.GetInt32(1, 101);
+int numeroadi;
+
+
+Console.WriteLine("--- Adivinhe o Número! ---");
+
+Console.WriteLine("Pensando em um Número...");
+
+Thread.Sleep(1000); 
+
+Console.WriteLine("Okay! Agora... ADIVINHE!");
+
